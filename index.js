99d64f82568fc383159575ea90f1540f2e6c1bb0
hizer00 <108923627+xxixiio@users.noreply.github.com>
2025-03-20 18:00:39 +0100

    Refactor album data retrieval to use a dedicated function for improved clarity and maintainability

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const readline = require("node:readline");
 const cheerio = require("cheerio");
 const https = require("https");
 
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -10,12 +11,43 @@ const rl = readline.createInterface({
 rl.question(`Paste UNTITLED.STREAM URL: `, async (url) => {
   url = "https://untitled.stream/library/project/8KqqxISbQrxrAoWY1WSxA";
 
-  const $ = await cheerio.fromURL(url);
-  const rawAlbumData = $('script:contains("tracks")').html();
-  const stringAlbumData = rawAlbumData.replace(/.*?=\s*|\s*;$/g, '');
-  const albumData = JSON.parse(stringAlbumData);
+  getAlbumData(url).then((data) => {
+    console.log(data);
+  });
 
-  console.log(albumData)
-  
   rl.close();
 });
+
+async function getAlbumData(url) {
+  const $ = await cheerio.fromURL(url);
+  let rawAlbumData = $('script:contains("tracks")').html();
+  // TODO: Better way to do the object parse.
+  rawAlbumData = JSON.parse(
+    rawAlbumData.replace("window.__remixContext = ", "").replace(";", "").trim()
+  );
+  let rawProject =
+    rawAlbumData.state.loaderData["routes/library.project.$projectSlug"].project
+      .project;
+  let rawTracks =
+    rawAlbumData.state.loaderData["routes/library.project.$projectSlug"].project
+      .tracks;
+
+  let albumData = {
+    title: rawProject.title,
+    artist_name: rawProject.artist_name,
+    cover_url: rawProject.artwork_url,
+    token: rawAlbumData.state.loaderData.root.env.SUPABASE_ANON_PUBLIC,
+    tracks: [],
+  };
+
+  rawTracks.forEach((track) => {
+    albumData.tracks.push({
+      title: track.title,
+      version_count: track.version_count,
+      audio_url: track.audio_url,
+      audio_fallback_url: track.audio_fallback_url,
+    });
+  });
+
+  return albumData;
+}
