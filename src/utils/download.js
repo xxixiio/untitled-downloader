e3697e93451a6fa31b9b6b30ee1d23e7a3aaa8c1
seb <108923627+xxixiio@users.noreply.github.com>
2025-12-03 13:13:30 +0100

    fix: update headers in generateDownloadUrl function for proper request handling

--- src/utils/download.js
+++ src/utils/download.js
@@ -15,10 +15,10 @@ function generateDownloadUrl(downloadablePath) {
         : `/${downloadablePath}`,
       method: "GET",
       headers: {
-        Host: "untitled.stream",
-        "Content-Type": "application/json",
-        "Content-Length": Buffer.byteLength(body, "utf8"),
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
         Accept: "*/*",
+        Host: "untitled.stream",
       },
     };
 
